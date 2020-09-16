bd5b5b9f8568d746d0ee82fa24e2b275b8715b4f
maybe <endymaybe@hotmail.com>
2020-09-16 20:24:48 +0800

    first vue

---
+++ src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login_container">
+    <div class="login_box"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.login_container {
+  height: 100%;
+  background-color: #2b4b6b;
+  color: #fff;
+}
+
+.login_box {
+  position: absolute;
+  width: 450px;
+  height: 300px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  border-radius: 3px;
+}
+</style>
